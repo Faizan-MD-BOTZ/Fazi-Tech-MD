8c36d6d5d4d7e8d10241d9b866ff7017df9c219e
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-11-11 23:40:58 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 // 🌟 Bot Configuration File 🌟
-// Edit these values to personalize your ARSLAN BOT
+// Edit these values to personalize your Fazi-Tech-Bot
 
 const settings = {
   // 🏷️ Sticker Settings
-  packname: '💎 ARSLAN-TECH-MD 💎',
-  author: '👑 ArslanMD Official 🇵🇰',
+  packname: '💎 Fazi-Tech-Bot 💎',
+  author: '👑 FAIZAN-MD Official 🇵🇰',
 
   // 🤖 Bot Identity
-  botName: '⚡ ARSLAN TECH MD ⚡',
-  botOwner: '🌍 ArslanMD Official 🇵🇰',   // Change to your name if needed
-  ownerNumber: '923237045919',        // Your number without + symbol
+  botName: '⚡ Fazi-Tech-Bot ⚡',
+  botOwner: '🌍 FAIZAN-MD Official 🇵🇰',   // Change to your name if needed
+  ownerNumber: '923266105873',        // Your number without + symbol
 
   // 🎬 APIs
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
@@ -18,7 +18,7 @@ const settings = {
   commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
 
   // 📝 Meta Information
-  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by ARSLAN POWER.',
+  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by FAIZAN POWER.',
   version: '2.0.0',
 updateZipUrl: "https://github.com/Arslan-MD/Arslan-Tech-MD/archive/refs/heads/main.zip",
   
